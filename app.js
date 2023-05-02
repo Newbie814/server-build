41bd5a9f93c0fefa3b7a4a291da9c09823f73a8a
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-05-01 22:03:02 -0400

    basic http server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Express Tutorial')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(
+    '<body style="background:black"><h2 style="color:white">Destiny is all</h2></body>'
+  );
+  res.end();
+});
+
+server.listen(5000, () => {
+  console.log('Server is running on port 3000');
+});
