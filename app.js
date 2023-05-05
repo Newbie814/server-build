7ab814cacb7392150930fff2df4200d882a2ce9e
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-05-04 23:17:47 -0400

    params and query strings

--- app.js
+++ app.js
@@ -25,7 +25,28 @@ app.get('/api/products/:productID', (req, res) => {
   if (!singleProduct) {
     return res.status(404).send('Product Does Not Exist');
   }
-  return res.json(singleProduct);
+  return res.status(200).json(singleProduct);
+});
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+  console.log(req.params);
+  res.send('hello world');
+});
+
+app.get('/api/v1/query', (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  res.send('hello query');
 });
 
 app.listen(port, () => {
