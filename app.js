e2d33854e7736fd6dfa4ee4bb51645b9e7091bed
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-05-05 20:46:10 -0400

    fixed bug - 'cannot set headers...'; need to add return in my conditions so code didn't keep running

--- app.js
+++ app.js
@@ -43,13 +43,13 @@ app.get('/api/v1/query', (req, res) => {
     });
   }
   if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
+    return (sortedProducts = sortedProducts.slice(0, Number(limit)));
   }
   if (sortedProducts.length < 1) {
     // res.status(200).send('no products matched your search');
     return res.status(200).json({ success: true, data: [] });
   }
-  res.status(200).json(sortedProducts);
+  return res.status(200).json(sortedProducts);
 });
 
 app.listen(port, () => {
