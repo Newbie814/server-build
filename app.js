b880d07001733e59ca22226708d6b338a5bbe5d9
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-05-05 21:20:05 -0400

    invoked middleware with app.use

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const logger = require('./logger-middleware.js');
+const port = 5000;
+
+// req => middleware => res
+
+app.use(logger);
+
+app.get('/', (req, res) => {
+  res.send('Home Page');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+app.get('/api/products', (req, res) => {
+  res.send('Products');
+});
+
+app.get('/api/items', (req, res) => {
+  res.send('Items');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
