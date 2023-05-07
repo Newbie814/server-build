f117e775c535989c2edcf70313c2c58f82ce65f4
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-05-07 13:29:21 -0400

    updated file names to match conventions

--- 09-thrid-party-middleware.js
+++ 09-thrid-party-middleware.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const app = express();
 const logger = require('./08-logger-middleware.js');
+const authorize = require('./authorize-middleware.js');
+const morgan = require('morgan');
 const port = 5000;
 
 // req => middleware => res
 
 // app.use(logger);  //invokes on all routes
 
-app.use('/api', logger); //invokes on all routes starting with /api
+// app.use('/api', logger); //invokes on all routes starting with /api
+app.use(morgan('tiny')); //third party middleware
+// app.use([logger, authorize]); //invokes multiple middlewares on all routes. executed in order
 
 app.get('/', (req, res) => {
   res.send('Home Page');
 });
 
 app.get('/about', (req, res) => {
+  console.log(req.user);
   res.send('About Page');
 });
 
