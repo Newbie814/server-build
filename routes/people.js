b7469f8d31b7664fdefb566a700caa548348ba4f
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-05-08 23:13:36 -0400

    setup controllers for routes

--- routes/people.js
+++ routes/people.js
@@ -2,61 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 
-let { people } = require('../data');
+const {
+  getPeople,
+  createPerson,
+  createPersonPostman,
+  updatePerson,
+  deletePerson,
+} = require('../controllers/router-controllers');
 
-router.get('/', (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
+router.get('/', getPeople);
 
-router.post('/', (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: 'please provide name value' });
-  }
-  res.status(201).json({ success: true, person: name });
-});
+router.post('/', createPerson);
 
-router.put('/:id', (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-  const person = people.find((person) => person.id === Number(id));
+router.put('/:id', updatePerson);
 
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with ${id}` });
-  }
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name;
-    }
-    return person;
-  });
-  res.status(200).json({ success: true, data: newPeople });
-});
+router.delete('/:id', deletePerson);
 
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  const person = people.find((person) => person.id === Number(id));
-  const newPeople = people.filter((person) => person.id !== Number(id));
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with ${id}` });
-  }
-  res.status(200).json({ success: true, data: newPeople });
-});
-
-router.post('/postman', (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: 'please provide name value' });
-  }
-  res.status(201).json({ success: true, data: [...people, name] });
-});
+router.post('/postman', createPersonPostman);
 
 module.exports = router;
